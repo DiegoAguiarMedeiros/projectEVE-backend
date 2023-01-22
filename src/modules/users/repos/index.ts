eb09530771773d3f2361534b4dcd04d12fd248ef
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-01-22 11:27:36 -0300

    add basic structure

--- src/modules/users/repos/index.ts
+++ src/modules/users/repos/index.ts
@@ -1,6 +1,6 @@
 
 import { SequelizeUserRepo } from "./implementations/sequelizeUserRepo";
-import models from "../../../shared/infra/database/sequilize/models";
+import models from "../../../shared/infra/database/sequelize/models";
 
 const userRepo = new SequelizeUserRepo(models);
 
