1f981204b249d82a83b1e6b8d3babd910d9123c1
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-02-21 16:44:31 -0300

    test should give error: Password doesnt meet criteria [8 chars min]

---
+++ src/modules/users/domain/userPassword.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from '@jest/globals';
+import { UserPassword } from './userPassword';
+
+
+describe('User', () => {
+  test('should give error: Password doesnt meet criteria [8 chars min]', () => {
+    const userPasswordOrError = UserPassword.create({ value: 'a' });
+    expect(userPasswordOrError.getErrorValue()).toBe('Password doesnt meet criteria [8 chars min].');
+  });
+})
