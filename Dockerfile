46e384482e2f261c1069a3fd48f1a96644bf4abe
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2025-01-17 19:23:49 -0300

    feat: remove startDocker.sh

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,9 @@ WORKDIR /usr/src/app
 
 COPY . .
 COPY ./.env.template ./.env
-RUN echo "npm install"
+
 RUN npm install
-RUN echo "npm run docker:up"
-RUN npm run docker:up
+RUN npm run build
 
 EXPOSE 3000
 
--- startDocker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Passo 1: Build das imagens do Docker com docker-compose
-echo "Building Docker images..."
-docker-compose build
-
-# Passo 2: Iniciar os containers com docker-compose
-echo "Starting Docker containers..."
-docker-compose up
